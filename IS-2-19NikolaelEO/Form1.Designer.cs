9f307519db4b9ceefa2323ddfd5458de9d63a2a9
Билибоба <@DESKTOP-BFM1G01>
2021-12-03 20:27:25 +0500

    Добавьте файлы проекта.

---
+++ IS-2-19NikolaelEO/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace IS_2_19NikolaelEO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(112, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(130, 27);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(109, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(245, 27);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(110, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(361, 27);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(107, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(474, 27);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(109, 20);
+            this.textBox5.TabIndex = 4;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(130, 53);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(566, 238);
+            this.listBox1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(130, 310);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 26);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(619, 310);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 26);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ IS-2-19NikolaelEO/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IS_2_19NikolaelEO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        abstract class Complect<l>
+        {
+            protected string cena;
+            protected string godvip;
+            
+            public Complect(string Cena, string Godvip)
+            {
+                cena = Cena;
+                godvip = Godvip;
+               
+            }
+            public abstract void Display(ListBox listbox1);
+        }
+        class Cp : Complect<string>
+        {
+            public string chastota1;
+            public string Yadra1;
+            public string kolvopotokov1;
+            public Cp(string Cena, string Godvipuska, string Chastota, string Kolvoiader, string Kolvopotokov)
+               : base(Cena, Godvipuska)
+            {
+                chast = Chastota;
+                kolvoiader = Kolvoiader;
+                kolvopotokov = Kolvopotokov;
+
+            }
+            public string chast { get { return chastota1; } set { chastota1 = value; } }
+            public string kolvoiader { get { return Yadra1; } set { Yadra1 = value; } }
+            public string kolvopotokov { get { return kolvopotokov1; } set { kolvopotokov1 = value; } }
+            public override void Display(ListBox listBox1)
+            {
+                listBox1.Items.Add($"Цена {cena},Год выпуска {godvip},Частота {chast},Количество ядер {Yadra1}, Количество потоков {kolvopotokov}");
+            }
+        }
+        class Videocarta : Complect<string>
+        {
+            public string chastota1;
+            public string proizvod1;
+            public string obiempameti1;
+
+            public Videocarta(string Cena, string Godvipuska, string Chastota, string Proizvod, string Obiempameti)
+               : base(Cena, Godvipuska)
+            {
+                chast = Chastota;
+                proizvod = Proizvod;
+                obiem = Obiempameti;
+
+            }
+            public string chast { get { return chastota1; } set { chastota1 = value; } }
+            public string proizvod { get { return proizvod1; } set { proizvod1 = value; } }
+            public string obiem { get { return obiempameti1; } set { obiempameti1 = value; } }
+            public override void Display(ListBox listBox1)
+            {
+                listBox1.Items.Add($"Цена{cena},Год выпуска{godvip},Частота {chast},Производитель {proizvod},Обьем памяти {obiem}");
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string cena = Convert.ToString(textBox1.Text);
+            string godvip = Convert.ToString(textBox2.Text);
+            string chastota = Convert.ToString(textBox3.Text);
+            string proizvod = Convert.ToString(textBox4.Text);
+            string obiem = Convert.ToString(textBox5.Text);
+            
+            Videocarta vid1 = new Videocarta(cena, godvip, chastota, proizvod, obiem);
+            vid1.Display(listBox1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string cena = Convert.ToString(textBox1.Text);
+            string godvip = Convert.ToString(textBox2.Text);
+            string chastota = Convert.ToString(textBox3.Text);
+            string kolvoiader = Convert.ToString(textBox4.Text);
+            string kolvopotokov = Convert.ToString(textBox5.Text);
+           
+            Cp cp1 = new Cp(cena, godvip, chastota, kolvoiader, kolvopotokov);
+            cp1.Display(listBox1);
+        }
+    }
+}
